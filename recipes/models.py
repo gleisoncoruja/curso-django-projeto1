12951c41796879eeaa72b9ab903919bf3c76d67b
Gleison Souza <gleison.lsouza@gmail.com>
2022-03-10 18:03:48 -0300

    aula 54

--- recipes/models.py
+++ recipes/models.py
@@ -1,19 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
-    slug = models.models.SlugField()
+    slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
     serving_unit = models.CharField(max_length=65)
-    preparation_steps = models.models.TextField()
-    prepartion_steps_is_html = models.BooleanField(defaut=False)
+    preparation_steps = models.TextField()
+    prepartion_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(defaut=False)
+    is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recippes/covers/%Y/%m/%d')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
