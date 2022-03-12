eda10e41ca2c828768da63335906aeed90cfa6f1
Gleison Souza <gleison.lsouza@gmail.com>
2022-03-12 15:40:31 -0300

    Aula 95

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -54,3 +54,13 @@ class RecipeModelTest(RecipeTestBase):
             recipe.is_published,
             msg='Recipe is_published is not False'
         )
+
+    def test_recipe_string_represetation(self):
+        needed = 'Testing Represetation'
+        self.recipe.title = needed
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(
+            str(self.recipe), needed,
+            msg='Recipe string represetation must be {needed}'
+        )
