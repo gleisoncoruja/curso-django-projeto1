ea97c98a46b14fbcdf6c87889751332122bea415
Gleison Souza <gleison.lsouza@gmail.com>
2022-03-14 19:36:21 -0300

    Aula 117

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -9,7 +9,7 @@ class PaginationTest(TestCase):
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=1,
-        )
+        )['pagination']
 
         self.assertEqual([1, 2, 3, 4], pagination)
 
@@ -19,7 +19,7 @@ class PaginationTest(TestCase):
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=1,
-        )
+        )['pagination']
 
         self.assertEqual([1, 2, 3, 4], pagination)
         # Current page =  2 - Qty page = 2 - Middle page = 2
@@ -27,7 +27,7 @@ class PaginationTest(TestCase):
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=2,
-        )
+        )['pagination']
 
         self.assertEqual([1, 2, 3, 4], pagination)
         # Current page =  3 - Qty page = 2 - Middle page = 2
@@ -36,6 +36,59 @@ class PaginationTest(TestCase):
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=3,
-        )
+        )['pagination']
 
         self.assertEqual([2, 3, 4, 5], pagination)
+
+    # middle
+    def test_make_sure_middle_ranges_are_correct(self):
+        # Current page =  10 - Qty page = 2 - Middle page = 2
+        # Here Range Should Change
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=10,
+        )['pagination']
+
+        self.assertEqual([9, 10, 11, 12], pagination)
+
+        # Current page =  12 - Qty page = 2 - Middle page = 2
+        # Here Range Should Change
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=12,
+        )['pagination']
+
+        self.assertEqual([11, 12, 13, 14], pagination)
+
+    def test_make_pagination_range_is_static_when_last_page_is_next(self):
+        # Current page =  19 - Qty page = 2 - Middle page = 2
+        # Here Range Should Change
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=19,
+        )['pagination']
+
+        self.assertEqual([17, 18, 19, 20], pagination)
+
+        # Current page =  20 - Qty page = 2 - Middle page = 2
+        # Here Range Should Change
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=20,
+        )['pagination']
+
+        self.assertEqual([17, 18, 19, 20], pagination)
+
+        # Current page =  21 - Qty page = 2 - Middle page = 2
+        # Here Range Should Change
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=21,
+        )['pagination']
+
+        self.assertEqual([17, 18, 19, 20], pagination)
