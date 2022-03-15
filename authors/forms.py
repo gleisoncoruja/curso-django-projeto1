27a0861d5563eca939175ea864c512fbb52977ad
Gleison Souza <gleison.lsouza@gmail.com>
2022-03-15 16:53:19 -0300

    Aula 135

--- authors/forms.py
+++ authors/forms.py
@@ -1,9 +1,49 @@
-from attr import attr
 from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr}{attr_new_val}'.strip()
+
+
+def add_placeholder(field, place_holder_val):
+    field.widget.attrs['placeholder'] = place_holder_val
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username here')
+        add_placeholder(self.fields['email'], 'Your email')
+        add_placeholder(self.fields['first_name'], 'Ex.: John')
+        add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password here'
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must be have at least one uppercase letter,'
+            'one lowercase letter and one numerber. '
+            'The length should be at least 8 characters'
+        )
+    )
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password here'
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+    )
+
     class Meta:
         model = User
         fields = [
@@ -33,9 +73,7 @@ class RegisterForm(forms.ModelForm):
         }
 
         widgets = {
-            'username': forms.TextInput(attrs={
-                'placeholder': 'Type your username here'
-            }),
+
             'password': forms.PasswordInput(attrs={
                 'placeholder': 'Type your password here',
             })
