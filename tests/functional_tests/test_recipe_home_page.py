38c3a49db8fc945dfbeda29d3d481529b2acd1da
Gleison Souza <gleison.lsouza@gmail.com>
2022-03-17 10:35:47 -0300

    Aula 165

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -5,17 +5,23 @@ from selenium.webdriver.common.by import By
 from utils.browser import make_chrome_browser
 
 
-class RecipeHomePageFunctionalTest(StaticLiveServerTestCase):
+class RacipeBaseFunctionalTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
     def sleep(self, seconds=5):
         time.sleep(seconds)
 
-    def test_the_test(self):
-        browser = make_chrome_browser()
-        browser.get(self.live_server_url)
-        self.sleep()
 
-        body = browser.find_element(By.TAG_NAME, 'body')
+class RecipeHomePageFunctionalTest(RacipeBaseFunctionalTest):
+
+    def test_recipe_home_page_without_recipes_not_found_messages(self):
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element(By.TAG_NAME, 'body')
 
         self.assertIn('Ainda não há receitas cadastradas', body.text)
-
-        browser.quit()
