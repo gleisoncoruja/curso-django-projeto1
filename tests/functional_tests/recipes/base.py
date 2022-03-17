66342cf06a29e3a17adde7f739e74ff572c4a0fd
Gleison Souza <gleison.lsouza@gmail.com>
2022-03-17 11:32:19 -0300

    Aula 168

--- tests/functional_tests/recipes/base.py
+++ tests/functional_tests/recipes/base.py
@@ -4,8 +4,10 @@ import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from utils.browser import make_chrome_browser
 
+from recipes.tests.test_recipe_base import RecipeMixin
 
-class RacipeBaseFunctionalTest(StaticLiveServerTestCase):
+
+class RacipeBaseFunctionalTest(StaticLiveServerTestCase, RecipeMixin):
     def setUp(self) -> None:
         self.browser = make_chrome_browser()
         return super().setUp()
--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,9 +1,11 @@
 
+import pytest
 from selenium.webdriver.common.by import By
 
-from base import RacipeBaseFunctionalTest
+from .base import RacipeBaseFunctionalTest
 
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RacipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipes_not_found_messages(self):
         self.browser.get(self.live_server_url)
