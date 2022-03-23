1338925a5194b9acee88a757a4c9e735f6ad341d
Gleison Souza <gleison.lsouza@gmail.com>
2022-03-23 16:57:20 -0300

    258. Adicionando a lógica de ocultar e mostrar o menu com JavaScript

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -1,4 +1,4 @@
-function my_scope() {
+(() => {
     const forms = document.querySelectorAll('.form-delete')
 
     for (const form of forms){
@@ -11,6 +11,37 @@ function my_scope() {
                 
         });
     }
-}
 
-my_scope();
+})();
+    
+    
+(() => {
+
+    const buttonCloseMenu = document.querySelector('.button-close-menu');
+    const buttonShowMenu =document.querySelector('.button-show-menu');
+    const menuContainer = document.querySelector('.menu-container');
+
+    const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+    const menuHiddenClass = 'menu-hidden';
+
+    const closeMenu = () => {
+        buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
+        menuContainer.classList.add(menuHiddenClass);
+    }
+
+    const showMenu = () => {
+        buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
+        menuContainer.classList.remove(menuHiddenClass);
+    }
+
+    if (buttonCloseMenu){
+        buttonShowMenu.removeEventListener('click', () => showMenu());
+        buttonCloseMenu.addEventListener('click', () => closeMenu());
+    }
+
+    if (buttonShowMenu){
+        buttonCloseMenu.removeEventListener('click', () => closeMenu());
+        buttonShowMenu.addEventListener('click', () => showMenu());
+    }
+
+})();
