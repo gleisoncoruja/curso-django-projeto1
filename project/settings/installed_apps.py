0dfad33b3e40ab254c02767d33d42bf294122b1b
Gleison Souza <gleison.lsouza@gmail.com>
2022-03-23 15:19:20 -0300

    254. Atualizando o chromedriver.exe e evitando erros com o Pytest

---
+++ project/settings/installed_apps.py
@@ -0,0 +1,15 @@
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Meus apps abaixo
+    'recipes',
+    'authors',
+    'tag',
+
+]
